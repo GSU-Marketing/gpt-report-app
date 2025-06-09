0418efe3400db31d07308722f30825a33e1d2cb9
GSU-Marketing <kemerson@gsu.edu>
2025-06-09 15:42:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -569,7 +569,9 @@ elif view == "Page 5: Geographic Insights":
 
     reader = get_geoip_reader()
     geo_df = get_enriched_geo_df(filtered_df, reader)
-    geo_df["region"] = geo_df["region"].apply(lambda x: us.states.lookup(x).name if us.states.lookup(x) else x)
+    geo_df["region"] = geo_df["region"].apply(
+        lambda x: us.states.lookup(str(x)).name if us.states.lookup(str(x)) else x
+    )
 
 
 
